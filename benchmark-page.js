72c35579f964da04fe1beb38869edf7acd69aa56
Otman Karim <71219931+Ott00@users.noreply.github.com>
2023-10-17 18:25:33 +0200

    Timer feature
    Added feature that go to next question when times ends

--- benchmark-page.js
+++ benchmark-page.js
@@ -116,131 +116,126 @@ const questions = [
 
 //TIMER
 window.onload = function () {
-  //timer
-  const FULL_DASH_ARRAY = 283;
-  const WARNING_THRESHOLD = 10;
-  const ALERT_THRESHOLD = 5;
-
-  const COLOR_CODES = {
-    info: {
-      color: "white"
-    },
-    warning: {
-      color: "orange",
-      threshold: WARNING_THRESHOLD
-    },
-    alert: {
-      color: "red",
-      threshold: ALERT_THRESHOLD
-    }
-  };
-
-  const TIME_LIMIT = 60;
-  let timePassed = 0;
-  let timeLeft = TIME_LIMIT;
-  let timerInterval = null;
-  let remainingPathColor = COLOR_CODES.info.color;
-
-  document.getElementById("timer").innerHTML = `
-  <div class="base-timer">
-    <svg class="base-timer__svg" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
-      <g class="base-timer__circle">
-        <circle class="base-timer__path-elapsed" cx="50" cy="50" r="45"></circle>
-        <path
-          id="base-timer-path-remaining"
-          stroke-dasharray="283"
-          class="base-timer__path-remaining ${remainingPathColor}"
-          d="
-            M 50, 50
-            m -45, 0
-            a 45,45 0 1,0 90,0
-            a 45,45 0 1,0 -90,0
-          "
-        ></path>
-      </g>
-    </svg>
-    <span id="seconds">SECONDS</span>
-    <span id="base-timer-label" class="base-timer__label">
-    
-    ${formatTime(timeLeft)}</span>
-    <span id="remaining-text">REMAINING</span>
-  </div>
-  `;
-
-  startTimer();
-
-  function onTimesUp() {
-    clearInterval(timerInterval);
-  }
-
-  function startTimer() {
-    timerInterval = setInterval(() => {
-      timePassed = timePassed += 1;
-      timeLeft = TIME_LIMIT - timePassed;
-      document.getElementById("base-timer-label").innerHTML =
-        formatTime(timeLeft);
-      setCircleDasharray();
-      setRemainingPathColor(timeLeft);
-
-      if (timeLeft === 0) {
-        onTimesUp();
-      }
-    }, 1000);
-  }
-
-  function formatTime(time) {
-    //const minutes = Math.floor(time / 60);
-    let seconds = time % 60;
-
-    if (seconds < 10) {
-      seconds = `0${seconds}`;
-    }
-
-    //return `${minutes}:${seconds}`;
-    return seconds;
-  }
-
-  function setRemainingPathColor(timeLeft) {
-    const { alert, warning, info } = COLOR_CODES;
-    if (timeLeft <= alert.threshold) {
-      document
-        .getElementById("base-timer-path-remaining")
-        .classList.remove(warning.color);
-      document
-        .getElementById("base-timer-path-remaining")
-        .classList.add(alert.color);
-    } else if (timeLeft <= warning.threshold) {
-      document
-        .getElementById("base-timer-path-remaining")
-        .classList.remove(info.color);
-      document
-        .getElementById("base-timer-path-remaining")
-        .classList.add(warning.color);
-    }
-  }
-
-  function calculateTimeFraction() {
-    const rawTimeFraction = timeLeft / TIME_LIMIT;
-    return rawTimeFraction - (1 / TIME_LIMIT) * (1 - rawTimeFraction);
-  }
-
-  function setCircleDasharray() {
-    const circleDasharray = `${(
-      calculateTimeFraction() * FULL_DASH_ARRAY
-    ).toFixed(0)} 283`;
-    document
-      .getElementById("base-timer-path-remaining")
-      .setAttribute("stroke-dasharray", circleDasharray);
-  }
   appendQuestionToPage(questionInfo);
 };
-//pesco una domanda random dalla lista
-// const getRandomQuestion = function () {
-//   const randomNumber = Math.floor(Math.random() * questions.length);
-//   //console.log(randomNumber);
-//   const question = questions[randomNumber];
-//   return question;
-// };
+//timer
+const FULL_DASH_ARRAY = 283;
+const WARNING_THRESHOLD = 10;
+const ALERT_THRESHOLD = 5;
+
+const COLOR_CODES = {
+  info: {
+    color: "white"
+  },
+  warning: {
+    color: "orange",
+    threshold: WARNING_THRESHOLD
+  },
+  alert: {
+    color: "red",
+    threshold: ALERT_THRESHOLD
+  }
+};
+
+const TIME_LIMIT = 10;
+let timePassed = 0;
+let timeLeft = TIME_LIMIT;
+let timerInterval = null;
+let remainingPathColor = COLOR_CODES.info.color;
+
+document.getElementById("timer").innerHTML = `
+<div class="base-timer">
+  <svg class="base-timer__svg" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+    <g class="base-timer__circle">
+      <circle class="base-timer__path-elapsed" cx="50" cy="50" r="45"></circle>
+      <path
+        id="base-timer-path-remaining"
+        stroke-dasharray="283"
+        class="base-timer__path-remaining ${remainingPathColor}"
+        d="
+          M 50, 50
+          m -45, 0
+          a 45,45 0 1,0 90,0
+          a 45,45 0 1,0 -90,0
+        "
+      ></path>
+    </g>
+  </svg>
+  <span id="seconds">SECONDS</span>
+  <span id="base-timer-label" class="base-timer__label">
+  
+  ${formatTime(timeLeft)}</span>
+  <span id="remaining-text">REMAINING</span>
+</div>
+`;
+
+startTimer();
+
+function onTimesUp() {
+  clearInterval(timerInterval);
+  goToNewQuestion();
+}
+
+function startTimer() {
+  timerInterval = setInterval(() => {
+    timePassed += 1;
+    timeLeft = TIME_LIMIT - timePassed;
+    const timerContainer = (document.getElementById(
+      "base-timer-label"
+    ).innerHTML = formatTime(timeLeft));
+    setCircleDasharray();
+    setRemainingPathColor(timeLeft);
+
+    if (timeLeft === 0) {
+      onTimesUp();
+    }
+  }, 1000);
+}
+
+function formatTime(time) {
+  //const minutes = Math.floor(time / 60);
+  let seconds = time % 60;
+
+  if (seconds < 10) {
+    seconds = `0${seconds}`;
+  }
+
+  //return `${minutes}:${seconds}`;
+  return seconds;
+}
+
+function setRemainingPathColor(timeLeft) {
+  const { alert, warning, info } = COLOR_CODES;
+  if (timeLeft <= alert.threshold) {
+    document
+      .getElementById("base-timer-path-remaining")
+      .classList.remove(warning.color);
+    document
+      .getElementById("base-timer-path-remaining")
+      .classList.add(alert.color);
+  } else if (timeLeft <= warning.threshold) {
+    document
+      .getElementById("base-timer-path-remaining")
+      .classList.remove(info.color);
+    document
+      .getElementById("base-timer-path-remaining")
+      .classList.add(warning.color);
+  }
+}
+
+function calculateTimeFraction() {
+  const rawTimeFraction = timeLeft / TIME_LIMIT;
+  return rawTimeFraction - (1 / TIME_LIMIT) * (1 - rawTimeFraction);
+}
+
+function setCircleDasharray() {
+  const circleDasharray = `${(
+    calculateTimeFraction() * FULL_DASH_ARRAY
+  ).toFixed(0)} 283`;
+  document
+    .getElementById("base-timer-path-remaining")
+    .setAttribute("stroke-dasharray", circleDasharray);
+}
 
 //imposto un punteggio globale dell'utente
 let globalUserPoint = 0;
@@ -255,12 +250,11 @@ const increaseCounterOfQuestions = function () {
   questionCounter.innerText = questionNumber + 1;
 };
 
+//pesco l'oggetto contenente la domanda
 const getQuestion = function () {
   const question = questions[questionNumber];
   return question;
 };
-
-//console.log(questionNumber);
 
 const questionInfo = getQuestion();
 //console.log(questionInfo);
@@ -345,6 +339,8 @@ const showResultsPage = function () {
 
 //spostarsi sulla nuova domanda
 const goToNewQuestion = function () {
+  console.log("Reset TIMER");
+  clearInterval(timerInterval);
   questionNumber++;
   //console.log("Question number" + questionNumber + "===" + questions.length);
   if (questionNumber > questions.length - 1) {
@@ -353,6 +349,9 @@ const goToNewQuestion = function () {
   } else {
     appendQuestionToPage(getQuestion());
     increaseCounterOfQuestions();
+    //resetto il timer ad ogni nuova risposta
+    timePassed = 0;
+    startTimer();
   }
 };
 
@@ -373,7 +372,6 @@ const selectAnswerAndChangeButtonColor = function (element) {
 const checkIfTheAnswerIsCorrect = function (element) {
   // console.log(element.innerText);
   // console.log(correctAnswer.innerText);
-  console.log("riga 375", correctAnswer);
   if (element.innerText === correctAnswer.innerText) {
     globalUserPoint++;
     console.log("Risposta corretta!");
@@ -388,3 +386,7 @@ answersContainer.addEventListener("click", (e) => {
   selectAnswerAndChangeButtonColor(element);
   checkIfTheAnswerIsCorrect(element);
 });
+
+const myTimeout = setTimeout(myGreeting, TIME_LIMIT * 1000);
+
+timerContainer.addEventListener("change", function () {});
