4bbc4df353a8d8d8f54e35fbb4863b343b0a2205
Alessia Palmieri <146003639+palmieri24@users.noreply.github.com>
2023-10-17 18:44:56 +0200

    FIX and WIP feedback page

--- feedback-page.js
+++ feedback-page.js
@@ -1,9 +1,9 @@
-let star = document.querySelectorAll("input");
+let star = document.querySelectorAll("input:not(#comment)");
 let showValue = document.querySelector("#rating-value");
 
 for (let i = 0; i < star.length; i++) {
   star[i].addEventListener("click", function () {
     i = this.value;
-    showValue.innerHTML = i + "out of 10";
+    showValue.innerText = i + "/10";
   });
 }
