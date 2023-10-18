7ee05906f6f68e25d3dbe14bfa9cc8401aead594
Giorgia Ipsaro Passione <145654021+GIPSaro@users.noreply.github.com>
2023-10-18 11:07:37 +0200

    modifiche grafico torta js

--- results.js
+++ results.js
@@ -11,14 +11,16 @@ function percent(partialValue, totalValue) {
 function percentWrong(partialValue, totalValue) {
   return [(totalValue - partialValue) / totalValue] * 100;
 }
-console.log(percent(2, 10));
 
 document.getElementById("percCorr").innerHTML = percent(data, 10) + "%";
-//document.getElementById("numCorr").innerHTML = data;
+document.getElementById("numCorr").innerHTML = data;
 document.getElementById("percWrong").innerHTML = percentWrong(data, 10) + "%";
-document.getElementById("numwrong").innerHTML = dataWrong;
+document.getElementById("numWrong").innerHTML = dataWrong;
 
 //qui bisonga impostare l'angolo del colore del grafico in base al risultato
+let angolo = dataWrong * 36;
+let torta = document.querySelector(".torta");
+torta.style.background = `conic-gradient(#d20094 ${angolo}deg , #00ffff ${angolo}deg )`;
 
 let result = document.querySelector("#torta-testo");
 function stampaResult(partialValue) {
@@ -27,6 +29,7 @@ function stampaResult(partialValue) {
     let print2 = document.createTextNode("Congratulations!");
     print1.appendChild(print2);
     let print3 = document.createElement("h3");
+
     let print4 = document.createTextNode("You passed the exam");
     print3.appendChild(print4);
     let newP = document.createElement("p");
@@ -37,12 +40,15 @@ function stampaResult(partialValue) {
     print3.appendChild(newP);
     print1.appendChild(print3);
     result.appendChild(print1);
+    print1.classList.add("h3Class");
+    print3.classList.add("h3SecondClass");
+    newP.classList.add("pClass");
   } else {
     let print1 = document.createElement("h3");
     let print2 = document.createTextNode("Unfortunate!");
     print1.appendChild(print2);
     let print3 = document.createElement("h3");
-    let print4 = document.createTextNode("You didn't passed the exam");
+    let print4 = document.createTextNode("You didn't pass the exam");
     print3.appendChild(print4);
     let newP = document.createElement("p");
     let print5 = document.createTextNode(
@@ -52,6 +58,9 @@ function stampaResult(partialValue) {
     print3.appendChild(newP);
     print1.appendChild(print3);
     result.appendChild(print1);
+    print1.classList.add("h3Class");
+    print3.classList.add("h3SecondClass");
+    newP.classList.add("pClass");
   }
 }
 stampaResult(percent(data, 10));
