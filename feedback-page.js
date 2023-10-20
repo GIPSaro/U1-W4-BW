db4f75cc0c7575d38dd25de586a3771911f8bff5
Giorgia Ipsaro Passione <145654021+GIPSaro@users.noreply.github.com>
2023-10-20 12:39:20 +0200

    new page FeebackArchive_pages
    add new page for feedback archive

--- feedback-page.js
+++ feedback-page.js
@@ -1,18 +1,35 @@
 let star = document.querySelectorAll("input:not(#comment)");
 let showValue = document.querySelector("#rating-value");
+let selectedRating = null;
 
 //logica per la selezione delle stelle di valutazione
 for (let i = 0; i < star.length; i++) {
   star[i].addEventListener("click", function () {
-    i = this.value;
-    showValue.innerText = i + "/10";
+    selectedRating = star[i].value;
+    showValue.innerText = selectedRating + "/10";
+  });
+
+  // al tasto invio per le stelle salvo il punteggio delle stelle
+  star[i].addEventListener("keyup", function (event) {
+    if (event.key === "Enter") {
+      selectedRating = star[i].value;
+      showValue.innerText = selectedRating + "/10";
+    }
   });
 }
 
-//evitiamo il comportamento di default del form alla pressione del tasto "enter"
-const inputComment = document.getElementById("comment");
-inputComment.addEventListener("keydown", (e) => {
-  if (e.key === "Enter") {
-    e.preventDefault();
+// al tasto invio per il commento salvo il commento
+
+const commentInput = document.getElementById("comment");
+commentInput.addEventListener("keyup", function (event) {
+  if (event.key === "Enter") {
+    event.preventDefault();
+    const commentText = commentInput.value.trim();
+
+    if (commentText !== "" && selectedRating) {
+      //memorizziamo il commento nello storage
+      localStorage.setItem("feedbackComment", commentText);
+      localStorage.setItem("feedbackRating", selectedRating);
+    }
   }
 });
